746fc13c83a8406a11195ea07f1cc7c60d2239a8
hadifayazi <hadifayazi4@gmail.com>
2023-03-07 00:08:24 +0100

    fix onDelete

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,8 @@
 import BookShow from "./BookShow";
 function BookList({ books, onDelete }) {
   const renderedBooks = books.map((book) => {
-    return <BookShow key={book.id} book={book} />;
+    return <BookShow key={book.id} book={book} onDelete={onDelete} />;
   });
-  return (
-    <div className="book-list">
-      {renderedBooks} onDelete={onDelete}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 }
 export default BookList;
